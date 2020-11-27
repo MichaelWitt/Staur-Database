eaff740c5f09474209810c49bd60535b09c05871
Annema Cozmoz <mikewittorp@gmail.com>
2020-11-27 12:21:55 -0500

    Updated file names and made folders for professionalism

--- db/schema.sql
+++ db/schema.sql
@@ -21,15 +21,3 @@ CREATE TABLE staur_employees (
   staur_name VARCHAR(30) NOT NULL,
   staur_leader VARCHAR(30) NOT NULL
 );
-
-INSERT INTO staur_department (department_name)
-VALUES ("Galaxy Defenders"), ("Ocean Heroes");
-
-INSERT INTO staur_role (title, salary)
-VALUES ("Space Officer", 75000), ("Treasure Finder", 64000);
-
-INSERT INTO staur_employees (first_name, last_name, staur_name, staur_leader)
-VALUES ("Destiny", "Swimins", "Xupernova", "Maximus Jones"), ("Rolly", "Juppy", "Dive Master", "Cheff Cuppins");
-
-SELECT * FROM staur_employees;
-
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO staur_department (department_name)
+VALUES ("Galaxy Defenders"), ("Ocean Heroes");
+
+INSERT INTO staur_role (title, salary)
+VALUES ("Space Officer", 75000), ("Treasure Finder", 64000);
+
+INSERT INTO staur_employees (first_name, last_name, staur_name, staur_leader)
+VALUES ("Destiny", "Swimins", "Xupernova", "Maximus Jones"), ("Rolly", "Juppy", "Dive Master", "Cheff Cuppins");
+
+SELECT * FROM staur_employees;
